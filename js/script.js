8c7a92564ee45388d44288ac00034a7047039e2a
Maksim2203 <98012684+Maksim2203@users.noreply.github.com>
2022-04-19 12:00:59 +0300

    т

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ closePopupCall.addEventListener('click',function(){
 
 // let popupDiscount = document.querySelector('popup-discount')
 // let openPopupDiscount = document.querySelector('popup-discount')
+
+const questions = document.querySelectorAll('.panel-group');
+
+questions.forEach((questions) => {
+    questions.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (e.target.closest('.panel-heading')){
+                const allContent = questions.querySelectorAll('.panel-collapse');
+                allContent.forEach((content) => {
+                    content.style.display = 'none';
+                })
+                let panel = e.target.closest('.panel');
+                let textPart = panel.querySelector('.panel-collapse');
+                textPart.style.display = 'block';
+            }
+        })
+    })
+    
